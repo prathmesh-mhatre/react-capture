3c9120b68d1dcc7bebdf9d017e911d645d250951
Prathmesh Mhatre <prathmesh.mhatre99@gmail.com>
2022-01-10 14:42:28 +0530

    3.Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,11 +2,13 @@ import React from "react";
 
 //importing components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
+      <ServicesSection />
     </>
   );
 };
