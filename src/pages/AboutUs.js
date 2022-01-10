9ca0f7ef2b535888cd924a9b66ccf7975cb02646
Prathmesh Mhatre <prathmesh.mhatre99@gmail.com>
2022-01-10 14:51:38 +0530

    4. FAQ Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,12 +3,14 @@ import React from "react";
 //importing components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
